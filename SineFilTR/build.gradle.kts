feffc102050fc2e842b9f561465075f1812137c3
q4ake <teknokolikweb@gmail.com>
2026-09-21 14:37:59 +0300

    chore: remove SineFilTR

--- SineFilTR/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 1
-cloudstream {
-    language = "tr"
-    description = "SineFilTR - Kaliteli Türkçe Film ve Dizi"
-    authors = listOf("ibrah")
-    status = 1
-    tvTypes = listOf("Movie", "TvSeries")
-    iconUrl = "https://sinefil.tv/favicon.ico"
-}
--- SineFilTR/src/main/kotlin/com/lagradost/SineFilTRPlugin.kt
+++
@@ -1,9 +0,0 @@
-package com.lagradost
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
-
-@CloudstreamPlugin
-class SineFilTRPlugin : Plugin() {
-    override fun load(context: Context) { registerMainAPI(SineFilTRProvider()) }
-}
